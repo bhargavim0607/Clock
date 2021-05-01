f9dc1c12ffdcf388749d65ffbd6068e783b52655
bhargavim0607 <68108779+bhargavim0607@users.noreply.github.com>
2021-05-01 19:12:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+function getTim(){
+    var date=new Date();
+    var h=check(date.getHours());
+    var m=check(date.getMinutes());
+    var s=check(date.getSeconds());
+    var d=date.getUTCDay();
+    document.getElementById(d).style.color = "red";
+    var month=check(new Number(date.getUTCMonth()+1));
+    var day=check(new Number(date.getUTCDate()));
+    document.getElementById("date").innerHTML="DATE:"+date.getUTCFullYear()+"-"+month+"-"+day;
+    
+    var time=h+":"+m+":"+s;
+
+    document.getElementById("time").innerHTML=time;
+    t=setTimeout(function(){ getTim()},500);
+}
+function check(h){
+    if(h<10){
+        h="0"+h;
+    }
+    return h;
+}
